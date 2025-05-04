888d46b1fbf240955a4f2dc5de19a6475b2f8ff9
Weever <109693401+Weever1337@users.noreply.github.com>
2025-05-04 21:58:13 +0300

    Refactored in some places main.rs and renamed get_content to get_initial_page_content just to understand.
    Changes:
    - unwrap_or(0) - fn get_max_page(...) -> u32;
    - next, and_then - fn get_max_page(...) -> u32;
    - static str for base url💀💀💀
    
    - changes in github.com (web) so I havent tested it

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use reqwest::{self, Error};
 use scraper::{Html, Selector};
 
-const BASE_URL: &'static str = "https://anon.services";
+const BASE_URL: &str = "https://anon.services";
 
 #[derive(Debug)]
 struct FileInfo {
@@ -13,37 +13,32 @@ fn search(query: &str, page: &str) -> String {
     format!("{BASE_URL}/search?q={}&page={}&sort=time_desc", query, page)
 }
 
-async fn get_content() -> Result<String, Error> {
+async fn get_initial_page_content() -> Result<String, Error> {
     let client = reqwest::Client::new();
-
     let resp = client.get(search("", "1")).send().await?.text().await?;
-
     Ok(resp)
 }
 
 fn get_max_page(document: &Html) -> u32 {
     let selector = Selector::parse("main.container > p:nth-child(4)").unwrap();
-    let mut max_pages: u32 = 0;
-
     document
         .select(&selector)
         .next()
-        .iter()
-        .for_each(|element| {
+        .and_then(|element| {
             let text = element.text().collect::<String>();
-            if let Some(pages_str) = text.split(" of ").last() {
-                if let Ok(pages) = pages_str
-                    .trim()
-                    .chars()
-                    .take_while(|char| char.is_ascii_digit())
-                    .collect::<String>()
-                    .parse::<u32>()
-                {
-                    max_pages = pages;
-                }
-            }
-        });
-    max_pages
+            text.split(" of ")
+                .last()
+                .and_then(|pages_str| {
+                    pages_str
+                        .trim()
+                        .chars()
+                        .take_while(|char| char.is_ascii_digit())
+                        .collect::<String>()
+                        .parse::<u32>()
+                        .ok()
+                })
+        })
+        .unwrap_or(0)
 }
 
 fn get_elements_on_page(document: &Html) -> Vec<FileInfo> {
@@ -51,7 +46,7 @@ fn get_elements_on_page(document: &Html) -> Vec<FileInfo> {
     let row_selector = Selector::parse("table.files-list > tbody > tr").unwrap();
     let link_selector = Selector::parse("td.ohidden.breaktext > a.decoration-none").unwrap();
 
-    for row_element in document.select(&row_selector) {
+    for row_element in document.select(&row_selector) { // тут вообще пиздец чуток, но ладно, иди нахуй
         if let Some(link_element) = row_element.select(&link_selector).next() {
             let name = link_element.text().collect::<String>().trim().to_string();
             if let Some(relative_link) = link_element.value().attr("href") {
@@ -68,21 +63,21 @@ fn get_elements_on_page(document: &Html) -> Vec<FileInfo> {
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
-    let content = get_content().await?;
-
+    let content = get_initial_page_content().await?;
     let doc = Html::parse_document(&content);
 
-    println!("{}", get_max_page(&doc));
-    get_elements_on_page(&doc)
-        .iter()
-        .enumerate()
-        .for_each(|(index, file_info)| {
-            println!(
-                "Index: {}\tName: {}, Link: {}",
-                index + 1,
-                file_info.name,
-                file_info.link
-            )
-        });
+    let max_pages = get_max_page(&doc);
+    println!("{}", max_pages);
+
+    let files = get_elements_on_page(&doc);
+    for (index, file_info) in files.iter().enumerate() {
+        println!( // ыыыы ванючы индекс ыыыы плаки плаки блять, почему \t?!!!
+            "Index: {}\tName: {}, Link: {}",
+            index + 1,
+            file_info.name,
+            file_info.link
+        );
+    }
+
     Ok(())
 }
